56b588278da61e33ff73fce800be8198b20d47c8
David Enke <david.enke@external.t-systems.com>
2023-02-22 18:21:14 +0100

    SEP-14859: removed unused rollup plugins

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -7,16 +7,8 @@ const copy = require('rollup-plugin-copy');
 const json = require('rollup-plugin-json');
 const replace = require('rollup-plugin-replace');
 const resolve = require('rollup-plugin-node-resolve');
-const url = require('rollup-plugin-url');
 
 const { dependencies, devDependencies } = require('./package.json');
-
-const SKIP_WARNINGS = [
-  'CIRCULAR_DEPENDENCY',
-  'EVAL',
-  'THIS_IS_UNDEFINED',
-  'UNRESOLVED_IMPORT'
-];
 const buildId = process.env.BUILD_ID || git.short();
 
 module.exports = {
@@ -28,19 +20,11 @@ module.exports = {
   ],
   input: 'src/server/index.js',
   output: { file: 'dist/server.js', format: 'cjs' },
-  moduleContext: {
-    'node_modules/react-icons/lib/esm/iconBase.js': 'global'
-  },
   plugins: [
     babel({ exclude: /node_modules/ }),
     json(),
     resolve(),
     commonjs(),
-    url({
-      limit: 5 * 1024,
-      publicPath: '/client/',
-      emitFiles: false
-    }),
     replace({
       'process.env.BUILD_ID': JSON.stringify(buildId)
     }),
